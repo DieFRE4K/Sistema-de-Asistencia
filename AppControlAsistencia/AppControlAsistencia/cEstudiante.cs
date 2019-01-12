70b6c7d43f412a4ce69e659ab1c15f5ed9280085
M1CHELCL <michelcanoloaiza3@gmail.com>
2019-01-12 17:23:38 -0500

    se añadio propiedades a la clase cEstudiante

--- AppControlAsistencia/AppControlAsistencia/cEstudiante.cs
+++ AppControlAsistencia/AppControlAsistencia/cEstudiante.cs
@@ -10,16 +10,36 @@ namespace AppControlAsistencia
     {
         //atributos
         private string Cod_Estudiante;
-        private string AP;
-        private string AM;
-        private string Nombres;
+        private string aAP;
+        private string aAM;
+        private string aNombres;
         //METODOS
         public cEstudiante(string Cod,string AP,string AM,string Nombres)
         {
             this.Cod_Estudiante = Cod;
-            this.Nombres = Nombres;
-            this.AP = AP;
-            this.AM = AM;
+            this.aNombres = Nombres;
+            this.aAP = AP;
+            this.aAM = AM;
+        }
+
+        public string Cod{
+        	get{return Cod_Estudiante;}
+        	set{Cod_Estudiante=value;}
+        }
+
+        public string Nombres{
+        	get{return aNombres;}
+        	set{aNombres=value;}
+        }
+
+        public string AP{
+        	get{return aAP;}
+        	set{aAP=value;}
+        }
+
+        public string AM{
+        	get{return aAM;}
+        	set{aAM=value;}
         }
 
     }
